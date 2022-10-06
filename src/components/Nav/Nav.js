0f2f9b1a5bd84479e9b607515d4d8ab9b5954ac0
sol <solrasido55@gmail.com>
2022-10-06 16:09:45 +0900

    ADD: Nav레이아웃 추가 - Nav.js 작성 - Nav.scss 작성

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import './Nav.scss';
 
 function Nav() {
-  return <></>;
+  return (
+    <nav className="nav-bar">
+      <div className="nav-container">
+        <div className="nav-logo">
+          <div className="nav-logo-img">
+            <img alt="instagram logo" src="../../images/instagram.png" />
+          </div>
+          <span>twostagram</span>
+        </div>
+        <div className="nav-search">
+          <span className="search-icon search-icon-default">
+            <i className="fas fa-search" />
+          </span>
+          <input type="text" placeholder="검색" className="search-input" />
+        </div>
+        <div className="nav-status">
+          <i className="far fa-compass" />
+          <i className="far fa-heart" />
+          <i className="far fa-user nav-profile-icon" />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
