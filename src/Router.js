15e490444cd3f0a986bed0a2f26a68be5b6c8b47
sol <solrasido55@gmail.com>
2022-10-06 14:12:10 +0900

    ADD : 초기 세팅 완료

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
-import Login from "./pages/Login/Login";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainDayeong from './pages/dayeongLee/Main/Main';
+import LoginDayeong from './pages/dayeongLee/Login/Login';
+import MainHyeonseok from './pages/hyeonseokJeong/Main/Main';
+import LoginHyeonseok from './pages/hyeonseokJeong/Login/Login';
+import MainSol from './pages/solKim/Main/Main';
+import LoginSol from './pages/solKim/Login/Login';
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/main" element={<Main />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-dayeong" element={<LoginDayeong />} />
+        <Route path="/main-dayeong" element={<MainDayeong />} />
+        <Route path="/login-hyeonseok" element={<LoginHyeonseok />} />
+        <Route path="/main-hyeonseok" element={<MainHyeonseok />} />
+        <Route path="/login-sol" element={<LoginSol />} />
+        <Route path="/main-sol" element={<MainSol />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
