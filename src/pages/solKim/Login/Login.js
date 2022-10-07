35e57e3c276d01bffece17125ed77f8e5f791e22
sol <solrasido55@gmail.com>
2022-10-07 11:39:31 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현 - id/password 인풋에 한글자 이상 존재하면 버튼이 활성화 - id는 @포함, password는 5자 이상이 될때만 버튼의 기능이 실행되게 구현

--- src/pages/solKim/Login/Login.js
+++ src/pages/solKim/Login/Login.js
@@ -1,10 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.scss';
-import Link from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function LoginSol() {
-  let [idInput, setIdInput] = useState('');
-  let [passwordInput, setPasswordInput] = useState('');
+  const [idInput, setIdInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [idValid, setIdValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [btnOnOff, setBtnOnOff] = useState('disabled-btn');
+
+  const navigate = useNavigate();
+
+  const saveUserId = e => {
+    setIdInput(e.target.value);
+  };
+  const saveUserPassword = e => {
+    setPasswordInput(e.target.value);
+  };
+
+  const btnHandler = () => {
+    idInput.length > 0 && passwordInput.length > 0
+      ? setBtnOnOff('')
+      : setBtnOnOff('disabled-btn');
+  };
+
+  const validationIdAndPassword = () => {
+    idInput.includes('@') ? setIdValid(true) : setIdValid(false);
+    passwordInput.length > 4 ? setPasswordValid(true) : setPasswordValid(false);
+  };
+
+  const moveToMain = () => {
+    if (btnOnOff === '') {
+      if (idValid === false) {
+        alert('이메일에 @를 포함시켜주세요');
+      } else if (passwordValid === false) {
+        alert('비밀번호는 5자 이상 입력하세요');
+      } else {
+        navigate('/main-sol');
+      }
+    }
+  };
+
+  useEffect(() => {
+    btnHandler();
+    validationIdAndPassword();
+  }, [idInput, passwordInput]);
 
   return (
     <div className="login-sol">
@@ -13,13 +53,20 @@ function LoginSol() {
           <header className="login-header">
             <span className="logo">Westagram</span>
           </header>
-          <form className="login-form">
+          <form
+            className="login-form"
+            onKeyDown={e => {
+              if (e.code === 'Enter') {
+                moveToMain();
+              }
+            }}
+          >
             <input
               type="text"
               className="id-input"
               placeholder="전화번호, 사용자 이름 또는 이메일"
               onChange={e => {
-                setIdInput(e.target.value);
+                saveUserId(e);
               }}
             />
             <input
@@ -27,10 +74,14 @@ function LoginSol() {
               className="password-input"
               placeholder="비밀번호"
               onChange={e => {
-                setPasswordInput(e.target.value);
+                saveUserPassword(e);
               }}
             />
-            <button type="button" className="login-btn disabled-btn">
+            <button
+              type="button"
+              className={`login-btn ${btnOnOff}`}
+              onClick={moveToMain}
+            >
               로그인
             </button>
           </form>
